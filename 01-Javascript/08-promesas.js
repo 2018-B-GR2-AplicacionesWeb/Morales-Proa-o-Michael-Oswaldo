604f06cb4ae73f18bb4296473f25931de3bf5e98
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2018-10-31 15:54:33 -0500

    comit clase 31/10/2018

---
+++ 01-Javascript/08-promesas.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+
+const promesa = (nombreArchivo) => {
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile(
+                nombreArchivo,
+                'utf-8',
+                (error, contenidoLeido) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(contenidoLeido);
+                    }
+                }
+            );
+        }
+    );
+};
+
+const promesaEscritura = (
+    nombreArchivo,
+    contenidoArchivo) => {
+    return new Promise(
+        (resolve, reject) => {
+            fs.writeFile(
+                nombreArchivo,
+                contenidoArchivo,
+                (error) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(contenidoArchivo);
+                    }
+                }
+            );
+        }
+    );
+};
+
+console.log(promesa);
+promesa('06-texto.txt')
+    .then(
+        (contenido) => {
+            console.log('Ok', contenido);
+            return promesaEscritura(
+                '06-texto.txt',
+                contenido + ' Nuevo Contenido');
+            // Promesa
+        }
+    )
+    .then(
+        (contenidoCompleto) => {
+            console.log(contenidoCompleto);
+        }
+    )
+    .catch(
+        (error) => {
+            console.log('Mal', error);
+        }
+    );
+
+
+
+
+
+//TRANSFORMACION DE APPENDFILE A PROMESA
+
+const appendFile =()=>{
+
+
+
+
+}
+
+
+
+
